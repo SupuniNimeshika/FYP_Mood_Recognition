e19257c808e167b45d5cda728c7dfb453f5a5284
Supuni Nimeshika <Supuni.Nimeshika@gmail.com>
2018-09-18 10:40:34 +0530

    Add new HappySad Data Set

--- Mood_Classification.py
+++ Mood_Classification.py
@@ -3,7 +3,6 @@ import string
 import nltk
 import pandas as pd
 
-# Read Csv File
 fullCorpus = pd.read_csv('SMSSpamCollection.tsv', sep='\t', header=None)
 fullCorpus.columns = ['label', 'body_text']
 fullCorpus.head()
@@ -18,19 +17,27 @@ print("Number of null in label: {}".format(fullCorpus['label'].isnull().sum()))
 print("Number of null in label: {}".format(fullCorpus['body_text'].isnull().sum()))
 
 
-# remove punctuation
-string.punctuation
-def remove_punct(text):
-    text_nopunct = "".join([char for char in text if char not in string.punctuation])
-    return text_nopunct
-fullCorpus['body_text_clean'] = fullCorpus['body_text'].apply(lambda x: remove_punct(x))
-fullCorpus.head(25)
+# # Read Csv File
+# fullCorpus = pd.read_csv('HappySadDataSet.tsv', sep='\t', header=None)
+# fullCorpus.columns = ['label', 'body_text']
+# fullCorpus.head()
+#
+# print("input data has {} rows columns".format(len(fullCorpus), len(fullCorpus.columns)))
+#
+# print("Out of {} rows , {} are happy , {} are sad".format(len(fullCorpus),
+#                                                          len(fullCorpus[fullCorpus['label'] == 'happy']),
+#                                                          len(fullCorpus[fullCorpus['label'] == 'sad'])))
+#
+# print("Number of null in label: {}".format(fullCorpus['label'].isnull().sum()))
+# print("Number of null in label: {}".format(fullCorpus['body_text'].isnull().sum()))
+
+
 
 # tokenization
 def tokenize(text):
     tokens = re.split('\W+', text)
     return tokens
-fullCorpus['body_text_tokenized'] = fullCorpus['body_text_clean'].apply(lambda x: tokenize(x.lower()))
+fullCorpus['body_text_tokenized'] = fullCorpus['body_text'].apply(lambda x: tokenize(x.lower()))
 fullCorpus.head()
 
 # stop word
@@ -40,3 +47,18 @@ def remove_stopwords(tokenized_list):
     return text
 fullCorpus['body_text_nostop'] = fullCorpus['body_text_tokenized'].apply(lambda x: remove_stopwords(x))
 fullCorpus.head()
+
+# lemmatizing
+wn = nltk.WordNetLemmatizer()
+
+def clean_text(tokeized_text):
+    text = [wn.lemmatize(word) for word in tokeized_text]
+    return text
+
+def join_text(sentence):
+    return ' '.join(sentence)
+
+def data_lematization():
+    fullCorpus['body_text_lemmatized'] = fullCorpus['body_text_nostop'].apply(lambda x: clean_text(x))
+    fullCorpus['body_text_lemmatized'] = fullCorpus['body_text_lemmatized'].apply(lambda x: join_text(x))
+data_lematization()
