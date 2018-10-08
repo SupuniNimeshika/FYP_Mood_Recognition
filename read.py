c966c0fbe04566e631b88205f19e1f0e2bff773d
Supuni Nimeshika <Supuni.Nimeshika@gmail.com>
2018-10-08 18:58:51 +0530

    Create Simple Code for Few Data Set

---
+++ read.py
@@ -0,0 +1,11 @@
+with open('mood.tsv') as f:
+    contents = f.readlines()
+# you may also want to remove whitespace characters like `\n` at the end of each line
+contents = [x.strip() for x in contents]
+for content in contents:
+    mood =content.split(' ',1)[0]
+    comment = (content.split(' ',1)[1])
+    sen = mood+'|'+comment
+    print(sen)
+    with open('new.tsv','a') as file:
+        file.write(sen+'\n')
